731b68a4d1abf97ae3174f7324a1c225f2b960ca
何康 <hekang19921111@hotmail.com>
2024-08-03 16:58:49 +0800

    add:新增上报

---
+++ PesoAce/LaunchVc/PLABaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PLABaseViewController.swift
+//  PesoAce
+//
+//  Created by apple on 2024/8/3.
+//
+
+import UIKit
+
+class PLABaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PesoAce/LaunchVc/PLALaunchViewController.swift
+++ PesoAce/LaunchVc/PLALaunchViewController.swift
@@ -7,14 +7,13 @@
 
 import UIKit
 
-class PLALaunchViewController: UIViewController {
+class PLALaunchViewController: PLABaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         panduanWangLuoWork()
-        view.backgroundColor = .white
     }
 }
 
