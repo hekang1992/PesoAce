152e8782dc102f868920101dae19292a93169c34
何康 <hekang19921111@hotmail.com>
2024-08-17 14:48:37 +0800

    add:新增无数据页面

--- PesoAce/Home/Controller/PLAChangeBankViewController.swift
+++ PesoAce/Home/Controller/PLAChangeBankViewController.swift
@@ -15,6 +15,11 @@ class PLAChangeBankViewController: PLABaseViewController {
         return cbankView
     }()
     
+    lazy var emptyView: PLAEmptyView = {
+        let emptyView = PLAEmptyView()
+        return emptyView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -47,7 +52,7 @@ extension PLAChangeBankViewController {
         PLAAFNetWorkManager.shared.requestAPI(params: ["isbank": "1", "change": "1", "wa": "6"], pageUrl: "/ace/looked/dello/there", method: .post) { [weak self] baseModel in
             ViewHud.hideLoadView()
             if baseModel.greasy == 0 || baseModel.greasy == 00 {
-                if let model = JSONDeserializer<wallpaperModel>.deserializeFrom(dict: baseModel.wallpaper), let modelaRR = model.cleaner {
+                if let model = JSONDeserializer<wallpaperModel>.deserializeFrom(dict: baseModel.wallpaper), let modelaRR = model.cleaner, modelaRR.count > 0 {
                     let sections = modelaRR.map { model -> CleanerSectionModel in
                         let millah = model.millah ?? ""
                         let items = model.improvement ?? []
@@ -55,11 +60,27 @@ extension PLAChangeBankViewController {
                     }
                     self?.cbankView.modelArray.accept(sections)
                     self?.cbankView.tableView.reloadData()
+                }else {
+                    self?.addemptyView()
                 }
+            }else {
+                self?.addemptyView()
             }
-        } errorBlock: { error in
+        } errorBlock: { [weak self] error in
+            self?.addemptyView()
             ViewHud.hideLoadView()
         }
+    }
+    
+    func addemptyView () {
+        DispatchQueue.main.async {
+            self.cbankView.addSubview(self.emptyView)
+            self.emptyView.snp.makeConstraints { make in
+                make.left.bottom.right.equalToSuperview()
+                make.top.equalTo(self.cbankView.titltLabel.snp.bottom).offset(8.px())
+            }
+        }
+        
     }
     
 }
